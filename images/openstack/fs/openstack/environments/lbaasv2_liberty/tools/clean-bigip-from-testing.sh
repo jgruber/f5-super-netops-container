5fceec9deca595319e6865294f6172450a2e21aa
John Gruber <john.t.gruber@gmail.com>
2017-05-31 10:19:26 -0500

    got rid of liberty and OSP specific testing

--- images/openstack/fs/openstack/environments/lbaasv2_liberty/tools/clean-bigip-from-testing.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-python $DIR/clean_bigip.py --config-file=$DIR/../etc/f5-agent.conf
-
--- images/openstack/fs/openstack/environments/lbaasv2_redhat_ospv9/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-NOW=$(date +%Y_%m_%d_%H_%M_%S)
-tempest run -t --whitelist-file $DIR/redhat_ospv9_whitelist.txt
-testr last --subunit | subunit2junitxml --output-to=${WORKINGDIRECTORY}/reports/${ENV}_${NOW}.xml
